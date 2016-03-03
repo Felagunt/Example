82bce8f0ba3e76a4978513b655a5a5f824e0199e
Mezin Ilya <felagunt@live.ru>
2016-03-03 14:33:33 +0300

    pointer

---
+++ Memory/Memory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//указатели
+namespace Memory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int* x;
+                int y = 10;
+                x = &y;
+                //получаем адресс переменной y
+                uint addr = (uint)x;
+                Console.WriteLine("Adress y: {0}", addr);
+                
+                byte* bytePointer = (byte*)addr + 4;//указатель на следующий байт поле addr
+                Console.WriteLine("Value int {0} from {1}", addr + 4, *bytePointer);
+                //обратная операция
+                uint oldAddr = (uint)bytePointer - 4;
+                int* intPointer = (int*)oldAddr;
+                Console.WriteLine("Value int {0} from {1}", oldAddr, *intPointer);
+                //преобразоваие  в double
+                double* doublePointer = (double*)addr + 4;
+                Console.WriteLine("Value double {0} from {1}", addr+4, *doublePointer);
+            }
+          
+
+        }
+    }
+}
